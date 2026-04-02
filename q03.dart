35849e8a206ced119ca47e3f07d1055faf326224
Júlia Carolina <juliacarolinabispo@gmail.com>
2026-04-02 10:14:55 -0300

    Exclusão da q03.dart

--- q03.dart
+++
@@ -1,7 +0,0 @@
-// Questão 3
-// Faça um algoritmo que imprima a série abaixo, o número de termos será lido.
-// S = 1 5 100 2 10 90 4 15 80 8 20 70 16 25 60 32 30 50 64 35 40 128 40 30 256 ...
-
-void main() {
-  // ...implemente aqui a solução quando for fazer
-}
